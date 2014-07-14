ec81e1fb65cbf173d9850dae7e10e1def4af3bb4
Tellechea <arthurtellechea@hotmail.com>
2014-07-14 14:53:17 -0300

    testando.java
    testando update

--- UsandoGIT/src/Main.java
+++ UsandoGIT/src/Main.java
@@ -4,10 +4,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int i = 0;
-		while (true){
+		while (i<=100){
 			i+=1;
 		}
-
+	System.out.println(i);
 	}
 
 }
